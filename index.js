8d1e9b8026d4448e78f3c2d8eb94ea5c8e0565d8
AnirudhPV123 <131606729+AnirudhPV123@users.noreply.github.com>
2023-06-22 14:54:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const sidenav = document.querySelector('#sidenav')
 const style = getComputedStyle(sidenav)  //in this method we can call style directly from css file
 
-const menu = document.getElementById("menu")
 
 function menubtn() {
     const right = style.right
 
     if (right == "-250px") {
         sidenav.style.right = "0px"
-        menu.src="./Barber_Shop_img/close.png"
+        document.getElementById("menu").src = "./Barber_Shop_img/close.png";
     } else {
         sidenav.style.right = "-250px"
-        menu.src="./Barber_Shop_img/menu.png"
+        document.getElementById("menu").src = "./Barber_Shop_img/menu.png";
     }
 }
 
