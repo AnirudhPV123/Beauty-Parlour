3049529d2ca3820f44185ddd8dc8aee93b566d49
AnirudhPV123 <131606729+AnirudhPV123@users.noreply.github.com>
2023-06-22 14:48:21 +0530

    Update index.js small change ./

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function menubtn() {
 
     if (right == "-250px") {
         sidenav.style.right = "0px"
-        menu.src="/Barber_Shop_img/close.png"
+        menu.src="./Barber_Shop_img/close.png"
     } else {
         sidenav.style.right = "-250px"
-        menu.src="/Barber_Shop_img/menu.png"
+        menu.src="./Barber_Shop_img/menu.png"
     }
 }
 
